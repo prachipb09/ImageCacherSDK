598ba46358f5fa5403f71a2a7a4a3e075df42a47
Prachi Bharadwaj <prachipb09@gmail.com>
2025-08-02 13:12:52 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,19 +2,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "ImageDownloaderBinarySDK", // 🚨 Rename to avoid conflict
+    name: "ImageDownloaderSDK",
     platforms: [
         .iOS(.v15)
     ],
     products: [
         .library(
-            name: "ImageDownloaderBinarySDK", // 🚨 Also rename the product
-            targets: ["ImageDownloaderBinaryTarget"] // ✅ Use new target name
+            name: "ImageDownloaderSDK",
+            targets: ["ImageDownloaderSDK"]
         )
     ],
     targets: [
         .binaryTarget(
-            name: "ImageDownloaderBinaryTarget", // ✅ Must not match anything else
+            name: "ImageDownloaderSDK",
             url: "https://github.com/prachipb09/ImageCacherSDK/releases/download/1.0.0/ImageDownloaderSDK.xcframework.zip",
             checksum: "53cc9dd08e9caa317d587ab820db27c586349c734508a9c176e26ac6cf89f5a7"
         )
